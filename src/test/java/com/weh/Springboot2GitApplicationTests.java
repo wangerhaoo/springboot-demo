2af0dda7585706e19a1442b18ce9159fd4ded01b
wangerhao <1532531258@qq.com>
2022-10-10 19:20:03 +0800

    project init

---
+++ src/test/java/com/weh/Springboot2GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
